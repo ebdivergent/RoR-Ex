bc9e04a6c6967069972b4f70a4425d7b7f95d028
ebdivergent <ebdivergent@gmail.com>
2021-03-05 11:59:54 +0200

    Task 1, Task2

---
+++ Task1/OrderInOnlineShop/OrderInOnlineShop/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OrderInOnlineShop
+{
+    class Order
+    {
+        public List<float> shopitems { get; set; }
+        public List<float> discounts { get; set; }
+
+        public float[] orderitem = { 100.0f, 112.2f, 301.1f };
+        public float[] discountitem = { 10.0f, 0.0f, 0.0f };
+
+        public Order(List<float> shopitems, List<float> discounts)
+        {
+            this.shopitems = shopitems;
+            this.discounts = discounts;
+        }
+
+        public float CalculateSum()
+        {
+            float sum = 0;
+            float temp;
+            float price;
+            
+            for(int i=0; i<orderitem.Length; i++)
+            {
+                temp = discountitem[i] / 100;
+                temp *= orderitem[i];
+                price = orderitem[i] - temp;
+
+                sum += price;
+            }
+
+            return sum;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order(new List<float>(), new List<float>());
+            float sum = order.CalculateSum();
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ Task2/TicTacToe/TicTacToe/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Linq;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isGame = true;
+            char player = 'X';
+            char[,] playBoard = new char[3, 3];
+            int playerMoves = 0;
+
+            while(isGame)
+            {
+                Start:
+                Console.Clear();
+
+                PrintBoard(playBoard);
+
+                Console.Write("Enter row: ");
+                int row = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter colum: ");
+                int colum = Convert.ToInt32(Console.ReadLine());                
+                
+                if(playBoard[row,colum] == 'X' || playBoard[row, colum] == 'O')
+                {
+                    Console.WriteLine("Cell isn`t empty");
+                    goto Start;
+                }
+
+                playBoard[row, colum] = player;
+
+                //Example for check a winner
+                if (player == playBoard[0, 0] && player == playBoard[0, 1] && player == playBoard[0,2])
+                {
+                    Console.WriteLine(player + " Won!");
+                    isGame = false;
+                }
+
+                playerMoves = playerMoves + 1;
+                //Example for check a drow
+                if(playerMoves == 9)
+                {
+                    Console.WriteLine("DRAW");
+                    isGame = false;
+                }
+
+                player = ChangeTurn(player);
+            }
+           
+        }
+
+
+        static char ChangeTurn(char currentPlayer)
+        {
+
+            if (currentPlayer == 'X')
+            {
+                return 'O';
+            }
+            else
+            {
+                return 'X';
+            }
+        }
+
+        static void PrintBoard(char [,] playBoard)
+        {
+            for (int row = 0; row < 3; row++)
+            {
+                Console.Write("| ");
+                for (int colum = 0; colum < 3; colum++)
+                {
+                    Console.Write(playBoard[row, colum]);
+                    Console.Write(" | ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
